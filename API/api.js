bf7c05e11bbcc32138fc234f738bed6fc0d0b105
CoreAnujDixit <anujdixitdseu@gmail.com>
2024-02-02 18:02:10 +0530

    Live News Added

--- API/api.js
+++ API/api.js
@@ -6,10 +6,16 @@
 //   return `${BASE_URL}/everything?apiKey=${apiKey}${queryParams}`;
 // };
 
+// export const BASE_URL = "https://newsapi.org/v2";
+
+// export const getNewsAPI = (keyword) => {
+//   const apiKey = "d2b2c276234c440db00653c7820257e1";
+//   return `${BASE_URL}/everything?apiKey=${apiKey}${keyword ? `&q=${encodeURIComponent("new")}` : ''}`;
+// };
+
 export const BASE_URL = "https://newsapi.org/v2";
 
-export const getNewsAPI = (keyword) => {
+export const getNewsAPI = (countryCode) => {
   const apiKey = "d2b2c276234c440db00653c7820257e1";
-  return `${BASE_URL}/everything?apiKey=${apiKey}${keyword ? `&q=${encodeURIComponent("technology")}` : ''}`;
+  return `${BASE_URL}/top-headlines?country=${'in'}&category=technology&apiKey=${apiKey}`;
 };
-
